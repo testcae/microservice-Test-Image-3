57ad5911072ce49a5f38318ac0b01fd452937735
ukimiawz <deathmoon123>
2017-10-24 09:05:48 +0200

    Generated microservice version 1.0

---
+++ db/Test_Image_3_create_tables.sql
@@ -0,0 +1,17 @@
+--
+-- Database Schema:  musicDb
+-- Automatically generated sql script for the service Test Image 3, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table imageDb.
+--
+CREATE TABLE musicDb.imageDb (
+  imageId VARCHAR(255) ,
+  imageName VARCHAR(255) ,
+CONSTRAINT imageId_PK PRIMARY KEY (imageId)
+ 
+);
+
+
+
